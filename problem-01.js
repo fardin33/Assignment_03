610b234a9fd352fbb8fffe20131b87ad163e8517
fardin33 <fardincse01@gmail.com>
2026-02-07 02:37:11 +0600

    Assignment-03, problem-01 ,Done

---
+++ problem-01.js
@@ -0,0 +1,50 @@
+// Problem-01: New Price for Eid Sale
+// ⚠️ Function Name Must be newPrice()
+// আসন্ন ইদ উপলক্ষে তুমি তোমার দোকানের প্রোডাক্টে ডিসকাউণ্ট দিতে চাচ্ছ। কিন্তু প্রতিটা প্রোডাক্টের নতুন দাম আলাদা আলাদা হিসাব করতে গেলে ঝামেলা। তাই তুমি একটা ফাংশন বানাতে চাও, যা মূল দাম এবং ডিসকাউন্ট% নিলে নতুন দাম বের করে দেবে।
+
+// Input :
+// প্রথম ইনপুট:  একটি সংখ্যা যা মূল দাম। currentPrice  হবে Number
+// দ্বিতীয় ইনপুট: একটি সংখ্যা যা ডিসকাউন্ট। discount হবে Number in  %
+
+// Output :   ফাংশনটি ডিসকাউন্ট করে নতুন দাম return করবে।  Output দশমিক সংখ্যা ও হতে পারে।  দশমিক এর পর ৩ ঘর  দেখাতে হবে।
+
+// Challenge 📢 : যদি কোনো ইনপুট সংখ্যা না হয় , অথবা discount এর পরিমান যদি 0  এর থেকে কম এবং 100 এর চেয়ে বেশি হয় তাহলে  Invalid স্ট্রিং  রিটার্ন করবে।
+
+// Hints 💡:  যদি একটি প্রোডাক্টের currentPrice = 1500 টাকা এবং discount = 20% হয়, তাহলে:
+// ডিসকাউন্টের পরিমাণ বের করো, ডিসকাউন্ট পরিমাণ = (1500 * 20) / 100 = 300 টাকা
+
+// SAMPLE INPUT
+// SAMPLE OUTPUT
+// 1500, 20
+// 1200.000
+// 2000, 15
+// 1700.000
+// 1200, 7
+// 1116.000
+// "1000", 10
+// Invalid
+// 2000, 17.17
+// 1656.600
+// 500, "5"
+// Invalid
+
+// Problem 01 - Solution : //
+
+function newPrice(currentPrice, discount) {
+  // Step - 1 : Input Value Check : //
+  if (typeof currentPrice !== "number" || typeof discount !== "number") {
+    return "Invalid";
+  } else if (discount < 0 || discount > 100) {
+    return "Invalid";
+  }
+
+  // Step - 2 : Get Final Discount Amount //
+  let discountAmount = (currentPrice * discount) / 100;
+  let number = currentPrice - discountAmount;
+
+  // Result 3 decimal Porjontto Output e Dekhabe :
+  return number.toFixed(3);
+}
+
+const getDiscount = newPrice(2000, 17.17);
+console.log(getDiscount);
